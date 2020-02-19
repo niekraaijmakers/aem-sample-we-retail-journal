a5db6628606917e1f4c9688231b1314bd31dc8e5
Niek Raaijmakers <niek@adobe.com>
2020-02-19 09:23:51 +0100

    Update readme with simple explanation

---
+++ react-app/README.md
@@ -0,0 +1,3 @@
+For aempack scripts to work properly, add to host file:
+
+local.we-retail-journal.com   127.0.0.1
--- react-app/deploy-to-io-dev.sh
+++
@@ -1,3 +0,0 @@
-npm run build-aem-prod-adobeio
-
-wskdeploy --apihost https://adobeioruntime.net --auth $AIO-AUTHKEY --namespace $AIO-NAMESPACE --manifest manifest.yaml
